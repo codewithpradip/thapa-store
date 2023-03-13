9b998c2944bc2dcbc9ddc01498d797f074f4bd2d
Pradip Thapa <pradipthapaofficial@gmail.com>
2023-03-13 16:06:00 +0545

    feat(frontend): implement react router.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import { Container } from 'react-bootstrap'
+import {BrowserRouter as Router, Routes, Route}  from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './pages/Home'
+import HomeScreen from './pages/HomeScreen'
+import ProductScreen from './pages/ProductScreen'
 
 function App() {
   return (
-    <div className="App">
+    <Router className="App">
       <Header/>
       <main className='py-3'>
         <Container>
-        <Home/>
+        <Routes>
+          <Route path='/' element={<HomeScreen/>} exact />
+        </Routes>
+        <Routes>
+          <Route path='/product/:id' element={<ProductScreen/>}/>
+        </Routes>
         </Container>
       </main>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import {Container, Navbar, Nav} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 function Header() {
   return (
     <header>
     <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/">Thapa Store</Navbar.Brand>
+        
+        <LinkContainer to='/'>
+        <Navbar.Brand >Thapa Store</Navbar.Brand>
+        </LinkContainer>
+        
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/cart"><i class="fa-solid fa-cart-shopping"></i>Cart</Nav.Link>
+
+          <LinkContainer to='/cart'>
+            <Nav.Link><i class="fa-solid fa-cart-shopping"></i>Cart</Nav.Link>
+          </LinkContainer>
+
+          <LinkContainer to='/login'>
             <Nav.Link href="/login"><i class="fa-solid fa-user"></i>Login</Nav.Link>
+          </LinkContainer>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
